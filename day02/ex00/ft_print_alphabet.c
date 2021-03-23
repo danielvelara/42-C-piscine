a7516eb5506d3fdb6d5c35281d8bc6260c07aac3
Daniel E. Velázquez Lara <danielvelara@gmail.com>
2021-03-23 12:28:36 -0600

    Update day02/ex00/ft_print_alphabet.c

--- day02/ex00/ft_print_alphabet.c
+++ day02/ex00/ft_print_alphabet.c
@@ -10,9 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void    ft_putchar(char c);
+void	ft_putchar(char c);
 
-void    ft_print_alphabet(void)
+void	ft_print_alphabet(void)
 {
 	char	letter;
 
